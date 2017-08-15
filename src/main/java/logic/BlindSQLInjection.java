b33126cf34fdaa8a2f903e055910f8d214957780
JaewoongMoon <mjw8585@gmail.com>
2017-08-16 02:26:03 +0900

    add prototype

--- src/main/java/logic/BlindSQLInjection.java
+++ src/main/java/logic/BlindSQLInjection.java
@@ -9,9 +9,41 @@ import java.net.URL;
 
 public class BlindSQLInjection {
 
+	private final String USER_AGENT = "Mozilla/5.0";
+	
 	public static void main(String[] args) {
 		
 		executePost("http://google.com", "");
+	}
+	
+	public String sendGet(String url) throws Exception {
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		return response.toString();
+
 	}
 	
 	public static String executePost(String targetURL, String urlParameters) {
